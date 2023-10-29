6a8e445e722d456693b1f92877073a5cdc0bd9a8
H M Abdul Fattah <111952077+hmfattah@users.noreply.github.com>
2023-10-29 00:27:12 -0700

    Update demo.py

--- implementation/demo.py
+++ implementation/demo.py
@@ -11,11 +11,12 @@ max_length = 128
 
 # Define a function to generate text
 def generate_text(input_text):
-    inputs = tokenizer(input_text, return_tensors="pt", padding="max_length", truncation=True, max_length=max_length)
+    input_ids = tokenizer.encode(input_text, return_tensors="pt", add_special_tokens=True)
     with torch.no_grad():
-        output = model.generate(**inputs)
+        output = model.generate(input_ids, max_length=len(input_ids[0]) + max_new_tokens, num_return_sequences=1)
     decoded_output = tokenizer.decode(output[0], skip_special_tokens=True)
     return decoded_output
+    
 
 # Example usage
 input_text = "Translate this English text to French: 'Hello, how are you?'"
